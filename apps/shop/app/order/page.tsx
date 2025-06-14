13ce5af527a8635ba843a525a2236a70de477494
hbs9312 <hbs30832@naver.com>
2025-06-14 10:24:01 +0900

    fix: fetch 에러 수정

--- apps/shop/app/order/page.tsx
+++ apps/shop/app/order/page.tsx
@@ -2,7 +2,7 @@ import { getOrderList } from "@/src/features/order/api/getOrderList";
 import { OrderHistoryPage } from "@/src/features/order/OrderHistoryPage";
 
 export default async function OrderPage() {
-    const { data } = await getOrderList();
+    // const { data } = await getOrderList()
 
-    return <OrderHistoryPage initialOrders={data?.content ?? []} />;
+    return <OrderHistoryPage initialOrders={[]} />;
 }
