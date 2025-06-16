70c7a5cb4708760c9e536b6ec906f9bd5210e09b
ParkBible <p8yft8903@gmail.com>
2025-06-17 00:15:41 +0900

    chore: 리뷰 api 응답 타입에 맞게 수정
    - 리뷰 작성하다가 닫기 버튼 클릭 시 팝업 보이지 않는 문제 수정(z-index)

--- apps/shop/next.config.ts
+++ apps/shop/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     images: {
-        domains: ["images.unsplash.com", "cdn.builder.io"],
+        domains: ["images.unsplash.com", "cdn.builder.io", "801base.s3.ap-northeast-2.amazonaws.com"],
     },
 
     // 메인 페이지로 리다이렉트
