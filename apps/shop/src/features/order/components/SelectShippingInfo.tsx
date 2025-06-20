abbe25b9c37a7e6728da61f420e1706a9ee2dd17
hbs9312 <hbs30832@naver.com>
2025-06-21 03:48:14 +0900

    fix: 배송지 관리 수정

--- apps/shop/src/features/order/components/SelectShippingInfo.tsx
+++ apps/shop/src/features/order/components/SelectShippingInfo.tsx
@@ -4,6 +4,9 @@ import { useModal } from "@/src/shared/hooks/useModal";
 import AddressList from "./AddressList";
 import type { AddressType } from "@/src/features/order/types";
 import SelectDeliveryMessage from "./SelectDeliveryMessage";
+import EditAddress from "./EditAddress";
+import { useQueryClient } from "@tanstack/react-query";
+import { useToast } from "@/src/shared/hooks/useToast";
 
 interface SelectShippingInfoProps {
     shipingInfo: AddressType | null;
@@ -13,16 +16,25 @@ interface SelectShippingInfoProps {
 }
 export default function SelectShippingInfo({ shipingInfo, onChangeDeliveryMessage, onChangeAddress, addresses }: SelectShippingInfoProps) {
     const { openModal, closeModal, Modal } = useModal();
+    const { openModal: openAddAddressModal, closeModal: closeAddAddressModal, Modal: AddAddressModal } = useModal();
 
+    const queryClient = useQueryClient();
+    const { toast, ToastUI } = useToast();
     const handleChangeAddress = (address: AddressType) => {
         onChangeAddress(address);
         closeModal();
     };
 
+    const onCreateAddress = () => {
+        closeAddAddressModal();
+        toast({ message: "배송지가 추가되었습니다." });
+        queryClient.invalidateQueries({ queryKey: ["addresses"] });
+    };
+
     return (
         <div className="p-4 border border-gray-200 rounded-2xl">
             <h4 className="text-lg font-bold">배송지</h4>
-            {addresses.length > 0 && shipingInfo ? (
+            {shipingInfo ? (
                 <>
                     <div className="flex justify-between items-center mb-5">
                         <div className="flex items-center gap-2">
@@ -44,13 +56,25 @@ export default function SelectShippingInfo({ shipingInfo, onChangeDeliveryMessag
                     </div>
                 </>
             ) : (
-                <div className="flex justify-center items-center h-full">
-                    <p className="text-gray-500">배송지를 추가해주세요.</p>
+                <div className="text-center py-2">
+                    {addresses.length > 0 ? (
+                        <Button variant="outline" onClick={openModal} type="button" size="full">
+                            배송지 선택
+                        </Button>
+                    ) : (
+                        <Button variant="outline" onClick={openAddAddressModal} type="button" size="full">
+                            배송지 추가
+                        </Button>
+                    )}
                 </div>
             )}
             <Modal title="배송지 변경" onClickClose={closeModal}>
                 <AddressList onSelect={handleChangeAddress} selectMode={true} selectedAddressId={shipingInfo?.addressId} addresses={addresses} />
             </Modal>
+            <AddAddressModal title="배송지 추가" onClickClose={closeAddAddressModal}>
+                <EditAddress onComplete={onCreateAddress} />
+            </AddAddressModal>
+            {ToastUI}
         </div>
     );
 }
