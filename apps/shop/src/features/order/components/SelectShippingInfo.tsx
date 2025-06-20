662b0d23523c9f86861d33d4b94850bd53d81138
hbs9312 <hbs30832@naver.com>
2025-06-21 04:06:08 +0900

    fix: 배송지 삭제 무효화 수정

--- apps/shop/src/features/order/components/SelectShippingInfo.tsx
+++ apps/shop/src/features/order/components/SelectShippingInfo.tsx
@@ -4,9 +4,6 @@ import { useModal } from "@/src/shared/hooks/useModal";
 import AddressList from "./AddressList";
 import type { AddressType } from "@/src/features/order/types";
 import SelectDeliveryMessage from "./SelectDeliveryMessage";
-import EditAddress from "./EditAddress";
-import { useQueryClient } from "@tanstack/react-query";
-import { useToast } from "@/src/shared/hooks/useToast";
 
 interface SelectShippingInfoProps {
     shipingInfo: AddressType | null;
@@ -16,19 +13,10 @@ interface SelectShippingInfoProps {
 }
 export default function SelectShippingInfo({ shipingInfo, onChangeDeliveryMessage, onChangeAddress, addresses }: SelectShippingInfoProps) {
     const { openModal, closeModal, Modal } = useModal();
-    const { openModal: openAddAddressModal, closeModal: closeAddAddressModal, Modal: AddAddressModal } = useModal();
 
-    const queryClient = useQueryClient();
-    const { toast, ToastUI } = useToast();
     const handleChangeAddress = (address: AddressType) => {
         onChangeAddress(address);
         closeModal();
-    };
-
-    const onCreateAddress = () => {
-        closeAddAddressModal();
-        toast({ message: "배송지가 추가되었습니다." });
-        queryClient.invalidateQueries({ queryKey: ["addresses"] });
     };
 
     return (
@@ -41,7 +29,7 @@ export default function SelectShippingInfo({ shipingInfo, onChangeDeliveryMessag
                             <h3 className="font-bold text-base">{shipingInfo?.alias}</h3>
                             {shipingInfo?.isDefault && <span className="text-sm text-gray-500 bg-gray-100 rounded-sm px-2 py-1">기본 배송지</span>}
                         </div>
-                        <Button variant="outline" onClick={openModal}>
+                        <Button variant="outline" onClick={openModal} type="button">
                             배송지 변경
                         </Button>
                     </div>
@@ -57,24 +45,14 @@ export default function SelectShippingInfo({ shipingInfo, onChangeDeliveryMessag
                 </>
             ) : (
                 <div className="text-center py-2">
-                    {addresses.length > 0 ? (
-                        <Button variant="outline" onClick={openModal} type="button" size="full">
-                            배송지 선택
-                        </Button>
-                    ) : (
-                        <Button variant="outline" onClick={openAddAddressModal} type="button" size="full">
-                            배송지 추가
-                        </Button>
-                    )}
+                    <Button variant="outline" onClick={openModal} type="button" size="full">
+                        배송지 선택
+                    </Button>
                 </div>
             )}
             <Modal title="배송지 변경" onClickClose={closeModal}>
                 <AddressList onSelect={handleChangeAddress} selectMode={true} selectedAddressId={shipingInfo?.addressId} addresses={addresses} />
             </Modal>
-            <AddAddressModal title="배송지 추가" onClickClose={closeAddAddressModal}>
-                <EditAddress onComplete={onCreateAddress} />
-            </AddAddressModal>
-            {ToastUI}
         </div>
     );
 }
